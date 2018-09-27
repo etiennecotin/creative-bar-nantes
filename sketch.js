144708e0b9666cb85c98657476f34263b107c3c7
Clément Nv <clement.nev@gmail.com>
2018-09-27 13:31:50 +0200

    Firt commit : Base

--- sketch.js
+++ sketch.js
@@ -1,39 +1,89 @@
+var p;
+var parts = [];
+var r = 20;
+var l = 50;
+var bars = [];
+var son;
+function preload() {
+    son = loadSound('ouverture-bar.mp3');
+}
 
-    function setup() {
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  parts.push(new Particule((width/2)+0, (height/2)+0));
+  parts.push(new Particule((width/2)+r, (height/2)+r));
+  parts.push(new Particule((width/2)-r, (height/2)-r));
+  parts.push(new Particule((width/2)+r, (height/2)-r));
+  parts.push(new Particule((width/2)-r, (height/2)+r));
 
-        createCanvas(windowWidth, windowHeight);
+  bars.push(new Bar((100), (300)))
+  bars.push(new Bar((300), (200)))
+  bars.push(new Bar((500), (300)))
+  bars.push(new Bar((700), (600)))
+}
 
-
-    }
-
-    function draw() {
-        background(200, 100);
-
-
-
-    }
-
-    class Bar {
-        constructor(nb_queue) {
-
-
+function draw() {
+  background(0);
+    
+    for (var i = 0; i < bars.length; i++) {        
+        bars[i].update();
+        for (var j = 0; j < parts.length; j++) {
+            parts[j].update();
+            // console.log(parts[j].pos.dist(bars[i].pos));
+            if (dist(bars[i].pos.x, bars[i].pos.y, parts[j].pos.x, parts[j].pos.y) < 50) {
+                bars[i].inside();
+            } else {
+                bars[i].outside();
+            }
         }
-
-        update() {
-
-        }
-
-        draw() {
-
-
-        }
-
     }
+    
+     
+}
 
-    function doubleClicked() {
-
+  class Particule{
+    constructor(x , y) {
+        this.pos = createVector(x, y);
+        this.vit = createVector(0,0);
     }
-
-    function mouseReleased() {
-
+    
+    update() {
+        push();
+        this.pos.add(this.vit);
+        // if(mouseIsPressed) {
+            this.mouse = createVector(mouseX, mouseY);
+            this.vit = this.mouse.sub(this.pos).div(50);
+        // }
+        ellipse(this.pos.x, this.pos.y, r);
+        pop();
     }
+    draw() {      
+      
+      // ellipse(this.pos.x, this.pos.y, 50);
+      // ellipse(this.pos.x, this.pos.y, 30);
+    }
+    
+  }
+
+  class Bar{
+    constructor(x , y) {
+        this.pos = createVector(x, y);
+    }
+    update() {
+        rect(this.pos.x, this.pos.y, l, l);
+        
+    }
+    inside() {
+        son.play();
+        push();
+        fill('#fae');
+        rect(this.pos.x, this.pos.y, l, l);
+        pop();
+    }
+    outside() {
+        push();
+        fill('#fff');
+        rect(this.pos.x, this.pos.y, l, l);
+        pop();
+    }
+  }
