5e97425baff688515e20c37fba66004f85208d82
Etienne Cotin <etienne.cotin@gmail.com>
2018-09-27 13:23:45 +0200

    initial commit

---
+++ sketch.js
@@ -0,0 +1,39 @@
+
+    function setup() {
+
+        createCanvas(windowWidth, windowHeight);
+
+
+    }
+
+    function draw() {
+        background(200, 100);
+
+
+
+    }
+
+    class Bar {
+        constructor(nb_queue) {
+
+
+        }
+
+        update() {
+
+        }
+
+        draw() {
+
+
+        }
+
+    }
+
+    function doubleClicked() {
+
+    }
+
+    function mouseReleased() {
+
+    }
