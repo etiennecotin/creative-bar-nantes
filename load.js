5be22a0d4d7afecad3b0fbb2a489760f53ddb4ee
Etienne Cotin <etienne.cotin@gmail.com>
2018-09-27 18:16:00 +0200

    Update load data

--- load.js
+++ load.js
@@ -1,16 +1,30 @@
 
-
-
    base_url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?location=47.212305,%20-1.555840&type=bar&radius=600&key=AIzaSyDcTTk48WN-xeLgJB_YIfeRTCQ7i25Icsg';
 
     array_bar = [];
 
 
-    function getData() {
+    async function getData(callback) {
 
-        $.getJSON("./data.json", function (data) {
-            return data;
+        let data = null;
+        // $.getJSON("./data.json", function (element) {
+        //     // console.log(data)
+        //     data = element;
+        //
+        // });
+
+        $.ajax({
+            url: "./data.json",
+            dataType: 'json',
+            async: false,
+            success: function(json){
+                // return json;
+                data = json
+            }
         });
+
+        return await data;
+
 
         // response.then(function (result) {
         //     let data = result;
@@ -45,10 +59,8 @@
 
     async function getResponseApi(url) {
 
-        const response = await
-        fetch(url);
-        let data = await
-        response.json();
+        const response = await fetch(url);
+        return await response.json();
 
-        return data;
+        // return data;
     }
