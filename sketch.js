0632c08b2da24cbbd5c49d8646334d0d9f1f7c81
Clément Nv <clement.nev@gmail.com>
2018-10-04 10:10:41 +0200

    Feat : Reset size bar on click

--- sketch.js
+++ sketch.js
@@ -91,6 +91,9 @@ function draw() {
     for (let i = 0; i < bars.length; i++) {
         bars[i].update();
         parts[0].update();
+        if (reset) {
+            bars[i].l = 15;
+        }
         // console.log(parts[j].pos.dist(bars[i].pos));
         if (dist(bars[i].coor.x, bars[i].coor.y, parts[0].pos.x, parts[0].pos.y) < bars[i].l/1.5) {
             bars[i].inside();
