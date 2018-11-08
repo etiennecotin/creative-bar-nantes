6b522f37347bbf39e1b0012b6867a57207bdaf6c
Etienne Cotin <etienne.cotin@gmail.com>
2018-11-08 13:46:38 +0100

    add rotate for playing bar

--- public/Bar.js
+++ public/Bar.js
@@ -27,16 +27,17 @@ class Bar {
     update() {
         this.coor = myMap.latLngToPixel(this.pos.x, this.pos.y);
         push();
-        textAlign(CENTER);
-        textSize(10);
-        fill(0, 102, 153);
-        text(this.text, this.coor.x + 0, this.coor.y - 10);
+            textAlign(CENTER);
+            textSize(10);
+            fill(0, 102, 153);
+            text(this.text, 0, -10);
         pop();
         push();
-        if (this.coor.x != -100 && this.coor.y != -100) {
-            // console.log(this.coor.x);
-            rect(this.coor.x, this.coor.y, this.l, this.l);
-        }
+            translate(this.coor.x, this.coor.y);
+            if (this.coor.x != -100 && this.coor.y != -100) {
+                // console.log(this.coor.x);
+                rect(0, 0, this.l, this.l);
+            }
         pop();
 
         // rect(this.pos.x, this.pos.y, this.l, this.l);
@@ -80,16 +81,32 @@ class Bar {
     }
     bigger() {
         push();
-        fill(this.r, this.g, this.b);
-        colorR = this.r;
-        colorG = this.g;
-        colorB = this.b;
-        rect(this.coor.x, this.coor.y, this.l+rms*ampMouvementBar, this.l+rms*ampMouvementBar);
-        this.music.setVolume(0.3);
-        if (!this.music._playing) {
-            this.music.setVolume(1,1);
-            this.music.play();
-        }
+            fill(this.r, this.g, this.b);
+            colorR = this.r;
+            colorG = this.g;
+            colorB = this.b;
+            // rect(this.coor.x, this.coor.y, this.l+rms*ampMouvementBar, this.l+rms*ampMouvementBar);
+
+            translate(this.coor.x, this.coor.y);
+            if (barRotate){
+                let pos2 = p5.Vector.mult(pos.rotate(0.5), cos(angle));
+                // let pos2 = p5.Vector.mult(pos.rotate(0.0001), cos(angle/5));
+                translate(pos2.x*0.6, pos2.y*0.6);
+                rotate(angle);
+                rect(0, 0, this.l+rms*ampMouvementBar, this.l+rms*ampMouvementBar);
+
+                angle += 0.00019;
+                // angle += 0.029;
+                pos.rotate(0.005);
+                // pos.rotate(0.0017);
+            }else {
+                rect(0, 0, this.l+rms*ampMouvementBar, this.l+rms*ampMouvementBar);
+            }
+            this.music.setVolume(0.3);
+            if (!this.music._playing) {
+                this.music.setVolume(1,1);
+                this.music.play();
+            }
         pop();
     }
     lower() {
